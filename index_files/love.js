4d2db1c3abfc4e4797a556fab0e9fbb0ec56d790
Deng-Xiao <dxeecs@pku.edu.cn>
2019-02-06 21:16:02 +0800

    change some font

--- index_files/love.js
+++ index_files/love.js
@@ -163,13 +163,13 @@
             ctx.scale(scale, scale);
             ctx.moveTo(0, 0);
     	    ctx.lineTo(15, 15);
-    	    ctx.lineTo(75, 15);
+    	    ctx.lineTo(77, 15);
             ctx.stroke();
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "15px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("小猪猪亲启", 23, 16);
+            ctx.font = "16px 黑体,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
+            ctx.fillText("小猪猪亲启", 22, 16);
             ctx.restore();
         },
         clear: function() {
